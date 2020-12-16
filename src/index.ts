6861c3297c57c31262509d94bf2233d156255d85
mrsky1001 <nikita.nk16@yandex.ru>
2020-12-16 23:36:57 +0600

    update

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,8 @@
+import VoiceClass from "./voiceClass";
+
 const voiceHelper = (e:string) => {
     console.log(e)
+    console.log(VoiceClass)
 }
+
+export default voiceHelper
---
+++ src/voiceClass.ts
@@ -0,0 +1,13 @@
+class VoiceClass {
+    private _num: number;
+
+    constructor() {
+        this._num = 12
+    }
+
+    log() {
+        console.log(this._num)
+    }
+}
+
+export default VoiceClass
