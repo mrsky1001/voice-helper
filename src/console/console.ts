3141ec84427ae602e5d47e2a4988d1dc3b20e076
mrsky1001 <nikita.nk16@yandex.ru>
2021-01-15 12:56:30 +0600

    update

--- src/console/console.ts
+++ src/console/console.ts
@@ -2,13 +2,16 @@ import "./console.scss"
 import $ from "../common/import-jquery"
 import {strings} from "../constants/strings";
 import {consoleNames} from "./consoleNames";
+import ConsoleDriver from "./consoleDriver";
 
 class Console {
     private readonly _consoleElemDom
-    private readonly _parent
+    private readonly _parent: ConsoleDriver
     private readonly _MAX_SCROLL = 999999
+    private readonly _pullMessages: Array<string>
+    private _indexMessage: number
 
-    constructor(parent) {
+    constructor(parent: ConsoleDriver) {
         this._consoleElemDom = $(`
             <div id="${consoleNames.CONSOLE_ID}">
                 <div id="title-container">
@@ -24,12 +27,48 @@ class Console {
         `)
 
         this._parent = parent
-
+        this._pullMessages = []
+        this._indexMessage = 0
     }
 
     /**
      * The private methods
      */
+    private get indexMessage(): number {
+        this._indexMessage = this._pullMessages.length > 0 ?
+            this._pullMessages.length - 1 : 0
+        return this._indexMessage
+    }
+
+    private set indexMessage(val) {
+        this._indexMessage = val;
+    }
+
+    private get prevMessage(): string {
+        if (this._pullMessages.length > 0) {
+            if (this.indexMessage > 0)
+                this.indexMessage--
+            return this._pullMessages[this.indexMessage]
+        } else
+            return ""
+    }
+
+    private get nextMessage(): string {
+        if (this._pullMessages.length > 0) {
+            if (this.indexMessage < this._pullMessages.length - 1) {
+                this.indexMessage++
+                return this._pullMessages[this.indexMessage]
+            } else
+                return ""
+        } else
+            return ""
+    }
+
+    private pushMessage(text: string) {
+        this._pullMessages.push(text)
+        this.indexMessage = this._pullMessages.length - 1;
+    }
+
     private appendConsole() {
         const body = $("body")
 
@@ -47,15 +86,29 @@ class Console {
         const commandInput = $("#" + consoleNames.COMMAND_INPUT)
         const sendButton = $("#" + consoleNames.SEND_COMMAND)
 
+        const sendMessage = () => {
+            const message = String(commandInput.val()).replace("\n", "")
+
+            if (message.length > 1) {
+                this.pushMessage(message)
+                this.sendCommand(message)
+            } else
+                commandInput.val("")
+        }
+
         sendButton.on('click',
             (e) =>
-                this.sendCommand(String(commandInput.val()).replace(/[\n\t\r]/g,""))
+                sendMessage()
         )
 
-        commandInput.on('change focus keydown keyup',
+        commandInput.on('keyup',
             (e) => {
                 if (e.key === 'Enter') {
-                    this.sendCommand(e.target.value)
+                    sendMessage()
+                } else if (e.key === 'ArrowUp') {
+                    commandInput.val(this.prevMessage)
+                } else if (e.key === 'ArrowDown') {
+                    commandInput.val(this.nextMessage)
                 }
             }
         )
